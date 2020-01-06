47c4046176d49b8b56e4f326c2a799c30eb7e97f
Тимур <supercakeman@yandex.ru>
2020-01-06 19:34:41 +0500

    First-commit

---
+++ Assets/DemoObjController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoObjController : MonoBehaviour
+{
+    public List<GameObject> dotsDests;
+    private GameObject Dest;
+
+    public float demoObjSpeed;
+    public int curIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curIndex = 0;
+        Dest = dotsDests[curIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Dest.transform.position, Time.deltaTime * demoObjSpeed);
+        transform.LookAt(Dest.transform);
+
+        if (transform.position == Dest.transform.position)
+        {
+            if(curIndex != dotsDests.Count - 1)
+            {
+                curIndex += 1;
+                Dest = dotsDests[curIndex];
+            }
+            else
+            {
+                curIndex = 0;
+                Dest = dotsDests[curIndex];
+            }
+        }
+
+    }
+}
---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public float cameraMoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition.x);
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * cameraMoveSpeed, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * cameraMoveSpeed, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * cameraMoveSpeed, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * cameraMoveSpeed, Space.Self);
+        }
+        //Screen.height
+    }
+}
